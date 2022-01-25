3804194b9f4fab32cc6f33e83a0d6737cced8604
uniqwtf <93663728+uniqwtf@users.noreply.github.com>
2022-01-25 18:57:07 +1000

    Delete navbar.js

--- navbar.js
+++
@@ -1,9 +0,0 @@
-    var nav = document.querySelector('nav');
-
-    window.addEventListener('scroll', function () {
-      if (window.pageYOffset > 100) {
-        nav.classList.add('bg-light', 'shadow');
-      } else {
-        nav.classList.remove('bg-light', 'shadow');
-      }
-    });
